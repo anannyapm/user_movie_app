be8a8d4322e503c8d19b59d3244a7582d7fef125
Anannya P M <62205468+anannyapm@users.noreply.github.com>
2025-02-15 20:13:59 +0530

    Add comments

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -7,22 +7,26 @@ import '../core/models/movie_model.dart';
 class MovieProvider with ChangeNotifier {
   final MovieRepository repository;
   MovieProvider(this.repository);
+
+  ///variables
   final List<MovieModel> _movies = [];
   List<MovieModel> get movies => _movies;
-
   int _currentPage = 1;
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
+  ///functions
+  
+  //reset page
   resetCurrPage() {
     _currentPage = 1;
   }
 
+  //fetch movies
   Future<void> fetchTrendingMovies() async {
     if (_isLoading) return;
     _isLoading = true;
     notifyListeners();
-
     try {
       developer.log('Fetching movies $_currentPage');
       final newMovies = await repository.getMovies(_currentPage);
@@ -31,11 +35,11 @@ class MovieProvider with ChangeNotifier {
     } catch (e) {
       developer.log('Error fetching movies: $e');
     }
-
     _isLoading = false;
     notifyListeners();
   }
 
+  //fetch movie details
   Future<MovieModel?> fetchMovieDetails(int movieId) async {
     try {
       return await repository.fetchMovieDetails(movieId);
